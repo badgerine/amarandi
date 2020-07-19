8b2825238a804e20d619c971da66da7fb3e03671
sibusiso matsini <sbu.matsini@gmail.com>
2020-07-19 18:13:20 +0200

    S26#438 working with refs & useRef()

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -6,34 +6,39 @@ import './Search.css';
 const Search = React.memo(props => {
   const { onLoadIngredients } = props;
   const [enteredFilter, setEnteredFilter] = useState('');
+  const inputRef = useRef();
 
   //executes after every re/render cycle of this cycle | 
   //[] only rerun when changes are detected on the properties listed in the array => effectively componentDidMount().
-  useEffect(()=> {
-    const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
-    fetch('https://burger-builder-ed94e.firebaseio.com/ingredients.json'+query)
-    .then(response => {
-      return response.json();
-    }).then(responseData => {
-      console.log('responseData=', responseData);
-      const loadedIngredients = [];
-      for (let key in responseData) {
-        loadedIngredients.push({
-          id: key,
-          ...responseData[key]
-        });
+  useEffect(() => {
+    setTimeout(() => {
+      if (enteredFilter === inputRef.current.value) {
+        const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        fetch('https://burger-builder-ed94e.firebaseio.com/ingredients.json' + query)
+          .then(response => {
+            return response.json();
+          }).then(responseData => {
+            console.log('responseData=', responseData);
+            const loadedIngredients = [];
+            for (let key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                ...responseData[key]
+              });
+            }
+            // setIngredients(loadedIngredients.filter(ingredient => (ingredient.amount != null)));
+            onLoadIngredients(loadedIngredients.filter(ingredient => (ingredient.amount != null)));
+          });
       }
-      // setIngredients(loadedIngredients.filter(ingredient => (ingredient.amount != null)));
-      onLoadIngredients(loadedIngredients.filter(ingredient => (ingredient.amount != null)));
-    });
-  },[enteredFilter, onLoadIngredients]);
+    }, 800)
+  }, [enteredFilter, onLoadIngredients, inputRef]);
 
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" value={enteredFilter} onChange={event => setEnteredFilter(event.target.value)} />
+          <input ref={inputRef} type="text" value={enteredFilter} onChange={event => setEnteredFilter(event.target.value)} />
         </div>
       </Card>
     </section>
