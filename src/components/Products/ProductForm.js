ec6cf84065a6be4c65047d4f5ce5b324a3c69af7
sibusiso matsini <sbu.matsini@gmail.com>
2020-08-10 22:26:49 +0200

    add product details to product form

--- src/components/Products/ProductForm.js
+++ src/components/Products/ProductForm.js
@@ -6,13 +6,16 @@ import './ProductForm.css';
 
 let renderCount = 0;
 const ProductForm = React.memo(props => {
-  console.log('[ProductForm] renderCount=',++renderCount);
+  console.log('[ProductForm] renderCount=', ++renderCount);
   const [title, setTitle] = useState('');
-  const [amount, setAmount] = useState('');
+  const [type, setType] = useState('');
+  const [description, setDescription] = useState('');
+  const [year, setYear] = useState('');
+  const [price, setPrice] = useState('');
 
   const submitHandler = event => {
     event.preventDefault();
-    props.onAddProduct({title, amount})
+    props.onAddProduct({ title, type, description, year, price })
   };
 
   return (
@@ -21,18 +24,45 @@ const ProductForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" value={title} onChange={event => setTitle(event.target.value)}/>
+            <input type="text" id="title" value={title} onChange={event => setTitle(event.target.value)} />
           </div>
           <div className="form-control">
-            <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" value={amount} onChange={event => {
+            <label htmlFor="type">Type</label>
+            <input type="text" id="type" value={type} onChange={event => {
               const enteredValue = event.target.value;
-              setAmount(prevAmount => {
-                console.log('[App.return] react avails previous state, investigate: prevAmount=',prevAmount);
-                console.log('[App.return] event.target.value=', enteredValue);
-                // return event.target.value;
+              setType(prevType => {
                 return enteredValue;
-                })}}/>
+              })
+            }} />
+          </div>
+          <div className="form-control">
+            <label htmlFor="type">Description</label>
+            <input type="text" id="description" value={description} onChange={event => {
+              const enteredValue = event.target.value;
+              setDescription(prevDescription => {
+                return enteredValue;
+              })
+            }} />
+          </div>
+          <div className="form-control">
+            <label htmlFor="year">Year</label>
+            <input type="text" id="year" value={year} onChange={event => {
+              const enteredValue = event.target.value;
+              setYear(prevYear => {
+                return enteredValue;
+              })
+            }} />
+          </div>
+          <div className="form-control">
+            <label htmlFor="price">Price</label>
+            <input type="number" id="price" value={price} onChange={event => {
+              const enteredValue = event.target.value;
+              setPrice(prevPrice => {
+                console.log('[App.return] react avails previous state, investigate: prevPrice=', prevPrice);
+                console.log('[App.return] event.target.value=', enteredValue);
+                return enteredValue;
+              })
+            }} />
           </div>
           <div className="product-form__actions">
             <button type="submit">Add Product</button>
